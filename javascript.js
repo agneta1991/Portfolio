98f526a9f2933cfc43c95f7595d6db44be561679
agneta1991 <pupienytea@gmail.com>
2023-06-19 17:47:22 +0300

    javascript linters fixed

--- javascript.js
+++ javascript.js
@@ -1,38 +1,38 @@
-const menuIcon = document.getElementById("fa-bars");
-const menuExit = document.getElementById("fa-close");
-const menuContent = document.querySelectorAll(".menu-content > a");
-const menuPage = document.getElementById("menu-page");
+const menuIcon = document.getElementById('fa-bars');
+const menuExit = document.getElementById('fa-close');
+const menuContent = document.querySelectorAll('.menu-content > a');
+const menuPage = document.getElementById('menu-page');
 
 function menuIconFunction(event) {
-	event.preventDefault();
-	menuPage.style.display = "flex";
+  event.preventDefault();
+  menuPage.style.display = 'flex';
 }
 
 function exitFunction(event) {
-	event.preventDefault();
-  menuPage.style.display = "none";
+  event.preventDefault();
+  menuPage.style.display = 'none';
 }
 
 function scrollSection(event) {
-	event.preventDefault();
-	const sectionId = this.getAttribute("href");
-	const targetedSection = document.querySelector(sectionId);
-  targetedSection.scrollIntoView({ behavior: "smooth" });
-  menuPage.style.display = "none";
+  event.preventDefault();
+  const sectionId = this.getAttribute('href');
+  const targetedSection = document.querySelector(sectionId);
+  targetedSection.scrollIntoView({ behavior: 'smooth' });
+  menuPage.style.display = 'none';
 }
 
-menuIcon.addEventListener("touchstart", menuIconFunction);
-menuExit.addEventListener("touchstart", exitFunction);
+menuIcon.addEventListener('touchstart', menuIconFunction);
+menuExit.addEventListener('touchstart', exitFunction);
 // menuPage.addEventListener("touchstart", exitFunction);
 
 menuContent.forEach((individualSection) => {
-	individualSection.addEventListener("touchstart", scrollSection);
+  individualSection.addEventListener('touchstart', scrollSection);
 });
 
-menuIcon.addEventListener("click", menuIconFunction);
+menuIcon.addEventListener('click', menuIconFunction);
 menuExit.addEventListener('click', exitFunction);
 // menuPage.addEventListener('click', exitFunction);
 
 menuContent.forEach((individualSection) => {
-	individualSection.addEventListener("click", scrollSection);
+  individualSection.addEventListener('click', scrollSection);
 });
