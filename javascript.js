958442c90c136fdcc7d5ac17baa2d6e8968b1367
agneta1991 <pupienytea@gmail.com>
2023-06-19 14:31:24 +0300

    dom manipulation

--- javascript.js
+++ javascript.js
@@ -1,1 +1,34 @@
-console.log('working');
+let menuIcon = document.getElementById('fa-bars');
+let menuExit = document.getElementById('fa-xmark');
+let menuContent = document.querySelectorAll('.menu-content > a');
+
+menuIcon.addEventListener('touchstart', menuIconFunction);
+
+function menuIconFunction() {
+  document.getElementById('menu-page').style.display = 'flex';
+}
+
+menuExit.addEventListener('touchstart', exitFunction);
+
+function exitFunction() {
+  document.getElementById('menu-page').style.display = 'none';
+}
+
+menuContent.forEach(individualSection => {
+  individualSection.addEventListener('touchstart', menuScroll);
+
+  function menuScroll() {
+    if (individualSection.className === 'menu-portfolio') {
+      window.scrollTo(0, 400);
+    }
+    else if (individualSection.className === 'menu-about-myself') {
+      window.scrollTo(0, 800);
+    }
+    else if (individualSection.className === 'menu-contact-form') {
+      window.scrollTo(0, 600);
+    }
+  }
+}
+)
+
+
