008296d5ebd72b0925eea96db5255d0904c33708
agneta1991 <pupienytea@gmail.com>
2023-06-20 16:47:35 +0300

    edditing object file for desktopversion

--- object-data-javascript.js
+++ object-data-javascript.js
@@ -1,24 +1,34 @@
 export const projects = [
   {
 
-      project_name: ['Tonic'],
-      project_info: ['Canopy', 'Back End Dev', 2015, 'Facebook'],
+      project_name_mobile: 'Tonic',
+      project_name_desktop: 'Tonic',
+      project_info_mobile: ['Canopy', 'Back End Dev', 2015],
+      project_info_desktop: ['Facebook', 'Back End Dev', 2015],
       description_mobile: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      description_desktop: 'Exploring the future of media in Facebook\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
       description_popup: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-      image: ['images/Snapshoot_Portfolio.png'],
-      technologies: ['HTML', 'Ruby on rails', 'CSS', 'JavaScript'],
+      image_mobile: ['images/Snapshoot_Portfolio.png'],
+      image_desktop: ['images/Snapshoot_Portfolio.png'],
+      technologies_mobile: ['HTML', 'CSS', 'JavaScript'],
+      technologies_desktop:['HTML', 'Ruby on rails', 'CSS', 'JavaScript'],
       link: 'https://link-to-live-project.com',
       source: 'https://github.com/username/project-repository',
 
   },
   {
 
-      project_name: ['Multi-post Stories', 'Uber Navvigation'],
-      project_info: ['Canopy', 'Back End Dev', 2015, 'Uber', 'Lead Developer'],
+      project_name_mobile: 'Multi-post Stories',
+      project_name_desktop: 'Uber Navvigation',
+      project_info_mobile: ['Canopy', 'Back End Dev', 2015],
+      project_info_desktop: ['Uber', 'Lead Developer', 2015],
       description_mobile:'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      description_desktop: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
       description_popup: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-      image: ['images/Snapshoot_Portfolio_(1).png'],
-      technologies: ['HTML', 'Ruby on rails', 'CSS', 'JavaScript'],
+      image_mobile: 'images/Snapshoot_Portfolio_(1).png',
+      image_desktop: 'images/Snapshoot_Portfolio_(1).png',
+      technologies_mobile: ['HTML', 'CSS', 'JavaScript'],
+      technologies_desktop: ['HTML', 'Ruby on rails', 'CSS', 'JavaScript'],
       link: 'https://link-to-live-project.com',
       source: 'https://github.com/username/project-repository',
 
@@ -26,12 +36,17 @@ export const projects = [
 
   {
 
-      project_name: ['Tonic'],
-      project_info: ['Canopy', 'Back End Dev', 2015],
+      project_name_mobile: 'Tonic',
+      project_name_desktop: 'Tonic',
+      project_info_mobile: ['Canopy', 'Back End Dev', 2015],
+      project_info_desktop: ['Canopy', 'Back End Dev', 2015],
       description_mobile: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      description_desktop: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
       description_popup: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-      image: ['images/Snapshoot_Portfolio_(2).png', 'images/desktop-pictures/Snapshoot_Portfolio.png'],
-      technologies: ['HTML', 'CSS', 'JavaScript'],
+      image_mobile: 'images/Snapshoot_Portfolio_(2).png',
+      image_desktop: 'images/desktop-pictures/Snapshoot_Portfolio.png',
+      technologies_mobile: ['HTML', 'CSS', 'JavaScript'],
+      technologies_desktop:['HTML', 'CSS', 'JavaScript'],
       link: 'https://link-to-live-project.com',
       source: 'https://github.com/username/project-repository',
 
@@ -39,12 +54,17 @@ export const projects = [
 
   {
 
-      project_name: ['Multi-Post Stories'],
-      project_info: ['Canopy', 'Back End Dev', 2015, 'Facebook', 'Full Stack Dev'],
+      project_name_mobile: 'Multi-Post Stories',
+      project_name_desktop: 'Multi-Post Stories',
+      project_info_mobile: ['Canopy', 'Back End Dev', 2015],
+      project_info_desktop: ['Facebook', 'Full Stack Dev', 2015],
       description_mobile: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+      description_desktop: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
       description_popup: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-      image: ['images/Snapshoot_Portfolio_(3).png'],
-      technologies: ['HTML', 'Ruby on rails', 'CSS', 'JavaScript'],
+      image_mobile: 'images/Snapshoot_Portfolio_(3).png',
+      image_desktop:'images/Snapshoot_Portfolio_(3).png',
+      technologies_mobile: ['HTML', 'CSS', 'JavaScript'],
+      technologies_desktop:['HTML', 'Ruby on rails', 'CSS', 'JavaScript'],
       link: 'https://link-to-live-project.com',
       source: 'https://github.com/username/project-repository',
 
