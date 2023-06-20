c448383c45c514fcf9006860431c0bf6bfb6a5e8
agneta1991 <pupienytea@gmail.com>
2023-06-20 19:20:48 +0300

    fixing linters

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+/* all below is the dom manipulation to creat work section */
+/// /////////////////////////////////////////////////////////
+import { projects } from './object-data-javascript.js';
+
 const menuIcon = document.getElementById('fa-bars');
 const menuExit = document.getElementById('fa-close');
 const menuContent = document.querySelectorAll('.menu-content > a');
@@ -36,126 +40,104 @@ menuContent.forEach((individualSection) => {
   individualSection.addEventListener('click', scrollSection);
 });
 
-/* all below is the dom manipulation to creat work section*/
-////////////////////////////////////////////////////////////
-import { projects } from "./object-data-javascript.js";
-
-for (let i = 0; i < projects.length; i++) {
-  let div = document.createElement('div');
-  div.id = 'dynamic-project' + [i];
-  div.className = 'project project-'+(i+1);
+for (let i = 0; i < projects.length; i + 1) {
+  const div = document.createElement('div');
+  div.id = `dynamic-project${[i]}`;
+  div.className = `project project-${i + 1}`;
   workSection.appendChild(div);
 
-  let secondDiv = document.createElement('div');
-  secondDiv.id = 'container'+ i;
+  const secondDiv = document.createElement('div');
+  secondDiv.id = `container${i}`;
   secondDiv.className = 'container';
-  document.getElementById('dynamic-project'+ i).appendChild(secondDiv);
+  document.getElementById(`dynamic-project${i}`).appendChild(secondDiv);
 
-  let image = document.createElement('img');
-  image.id ='image'+ i;
+  const image = document.createElement('img');
+  image.id = `image${i}`;
   image.className = 'image-mobile';
   image.alt = 'Project picture';
   image.src = projects[i].image_mobile;
-  document.getElementById('container' + i).appendChild(image);
+  document.getElementById(`container${i}`).appendChild(image);
 
-  let dimage = document.createElement('img');
-  dimage.id ='image'+ i;
-  dimage.className = 'image-desktop'
+  const dimage = document.createElement('img');
+  dimage.id = `image${i}`;
+  dimage.className = 'image-desktop';
   dimage.alt = 'Project picture';
   dimage.src = projects[i].image_desktop;
-  document.getElementById('container' + i).appendChild(dimage);
+  document.getElementById(`container${i}`).appendChild(dimage);
 
-  let h3 = document.createElement('h3');
-  h3.id= 'client-name'+ i;
+  const h3 = document.createElement('h3');
+  h3.id = `client-name${i}`;
   h3.className = 'client-name li-mobile';
   h3.innerHTML = projects[i].project_name_mobile;
-  document.getElementById('container'+ i).appendChild(h3);
+  document.getElementById(`container${i}`).appendChild(h3);
 
-  let h3desktop = document.createElement('h3');
-  h3desktop.id = 'client-name-desktop' + i;
+  const h3desktop = document.createElement('h3');
+  h3desktop.id = `client-name-desktop${i}`;
   h3desktop.className = 'client-name client-name-desktop';
   h3desktop.innerHTML = projects[i].project_name_desktop;
-  document.getElementById('container'+ i).appendChild(h3desktop);
+  document.getElementById(`container${i}`).appendChild(h3desktop);
 
-  let ul = document.createElement('ul');
+  const ul = document.createElement('ul');
   const info = projects[i].project_info_mobile;
-  info.forEach(individualInfoitem => {
-    let li = document.createElement('li');
-    li.id = 'li'+i;
+  info.forEach((individualInfoitem) => {
+    const li = document.createElement('li');
+    li.id = `li${i}`;
     li.className = 'li-mobile';
     li.innerHTML = individualInfoitem;
     ul.appendChild(li);
   });
-  document.getElementById('container'+i).appendChild(ul);
+  document.getElementById(`container${i}`).appendChild(ul);
 
-  let desktopUl = document.createElement('ul');
+  const desktopUl = document.createElement('ul');
   const desktopinfo = projects[i].project_info_desktop;
-  desktopinfo.forEach(individualdesktop => {
-    let desktopli = document.createElement('li');
-    desktopli.id = 'desktopli'+i;
+  desktopinfo.forEach((individualdesktop) => {
+    const desktopli = document.createElement('li');
+    desktopli.id = `desktopli${i}`;
     desktopli.className = 'li-desktop';
     desktopli.innerHTML = individualdesktop;
     desktopUl.appendChild(desktopli);
   });
-  document.getElementById('container'+i).appendChild(desktopUl);
+  document.getElementById(`container${i}`).appendChild(desktopUl);
 
-  let p = document.createElement('p');
+  const p = document.createElement('p');
   p.className = 'about-project project-mobile';
   p.innerHTML = projects[i].description_mobile;
-  document.getElementById('container'+ i).appendChild(p);
+  document.getElementById(`container${i}`).appendChild(p);
 
-  let desktopp = document.createElement('p');
+  const desktopp = document.createElement('p');
   desktopp.className = 'about-project project-desktop';
   desktopp.innerHTML = projects[i].description_desktop;
-  document.getElementById('container'+ i).appendChild(desktopp);
+  document.getElementById(`container${i}`).appendChild(desktopp);
 
-  let technologyDiv = document.createElement('div');
-  technologyDiv.id = 'technologyDiv' + i;
+  const technologyDiv = document.createElement('div');
+  technologyDiv.id = `technologyDiv${i}`;
   technologyDiv.className = 'buttons';
-  document.getElementById('container'+ i).appendChild(technologyDiv);
+  document.getElementById(`container${i}`).appendChild(technologyDiv);
 
-  let technologyUl = document.createElement('ul');
-  let technoLi = projects[i].technologies_mobile;
+  const technologyUl = document.createElement('ul');
+  const technoLi = projects[i].technologies_mobile;
 
-  technoLi.forEach(individualTechLiItem => {
-    let secondLi = document.createElement ('li');
+  technoLi.forEach((individualTechLiItem) => {
+    const secondLi = document.createElement('li');
     secondLi.innerHTML = individualTechLiItem;
     technologyUl.appendChild(secondLi);
-  })
-  document.getElementById('technologyDiv' + i).appendChild(technologyUl);
+  });
+  document.getElementById(`technologyDiv${i}`).appendChild(technologyUl);
 
-  let btn = document.createElement('button');
-  btn.id = 'see-project'+(i+1);
+  const btn = document.createElement('button');
+  btn.id = `see-project${i + 1}`;
   btn.className = 'see-project';
   btn.type = 'submit';
   btn.innerHTML = 'See project';
-  document.getElementById('container'+ i).appendChild(btn);
+  document.getElementById(`container${i}`).appendChild(btn);
 
-  let clientName = document.getElementById('desktopli'+i);
+  const clientName = document.getElementById(`desktopli${i}`);
   clientName.classList.add('name');
-  let mobileClientName = document.getElementById('li'+i);
+  const mobileClientName = document.getElementById(`li${i}`);
   mobileClientName.classList.add('name');
 }
 
-let seeProjects = document.querySelectorAll('[id^="see-project"]');
-seeProjects.forEach(individualProject=>{
-  console.log(individualProject.id);
-})
-
-const seeProject1 = document.getElementById('see-project1');
-const seeProject2 = document.getElementById('see-project2');
-const seeProject3 = document.getElementById('see-project3');
-const seeProject4 = document.getElementById('see-project4');
-
-/*seeProject1.addEventListener('touchstart', popupFunction);
-seeProject2.addEventListener('touchstart', popupFunction);
-seeProject3.addEventListener('touchstart', popupFunction);
-seeProject4.addEventListener('touchstart', popupFunction);
-
-function popupFunction() {
-  let div = document.createElement('div');
-  div.id = 'dynamic-project';
-  div.className = 'project';
-  div.innerHTML = 'im here';
-  workSection.appendChild(div);
-}*/
+const seeProjects = document.querySelectorAll('[id^="see-project"]');
+seeProjects.forEach((individualProject) => {
+  individualProject.addEventListener('touchstart', popupFunction);
+});
