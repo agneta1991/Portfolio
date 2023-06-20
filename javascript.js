3979b6a4e3d241293a741b619fbb88dd52d283a0
agneta1991 <pupienytea@gmail.com>
2023-06-20 12:34:06 +0300

    fixed linters, added objects

--- javascript.js
+++ javascript.js
@@ -34,3 +34,45 @@ menuExit.addEventListener('click', exitFunction);
 menuContent.forEach((individualSection) => {
   individualSection.addEventListener('click', scrollSection);
 });
+
+const projects = {
+  project_one: {
+    project_name: ['Tonic'],
+    project_info: ['Canopy', 'Facebook', 'Back End Dev', 2015],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    image: ['images/Snapshoot_Portfolio.png'],
+    technologies: ['HTML', 'Ruby on rails', 'CSS', 'JavaScript'],
+    link: 'https://link-to-live-project.com',
+    source: 'https://github.com/username/project-repository',
+  },
+
+  project_two: {
+    project_name: ['Multi-post Stories', 'Uber Navvigation'],
+    project_info: ['Canopy', 'Uber', 'Back End Dev', 'Lead Developer', 2015],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    image: ['images/Snapshoot_Portfolio_(1).png'],
+    technologies: ['HTML', 'Ruby on rails', 'CSS', 'JavaScript'],
+    link: 'https://link-to-live-project.com',
+    source: 'https://github.com/username/project-repository',
+  },
+
+  project_three: {
+    project_name: ['Tonic'],
+    project_info: ['Canopy', 'Back End Dev', 2015],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    image: ['images/Snapshoot_Portfolio_(2).png', 'images/desktop-pictures/Snapshoot_Portfolio.png'],
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    link: 'https://link-to-live-project.com',
+    source: 'https://github.com/username/project-repository',
+  },
+
+  project_four: {
+    project_name: ['Multi-Post Stories'],
+    project_info: ['Canopy', 'Facebook', 'Back End Dev', 'Full Stack Dev', 2015],
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    image: ['images/Snapshoot_Portfolio (3).png'],
+    technologies: ['HTML', 'Ruby on rails', 'CSS', 'JavaScript'],
+    link: 'https://link-to-live-project.com',
+    source: 'https://github.com/username/project-repository',
+  },
+};
