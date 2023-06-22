3e783769e344999fd55a1abd899b2bc768f1a00a
agneta1991 <pupienytea@gmail.com>
2023-06-22 10:39:23 +0300

    linter error fix

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-
 const projects = [
   {
 
@@ -77,7 +76,7 @@ const projects = [
     link: 'https://link-to-live-project.com',
     source: 'https://github.com/username/project-repository',
 
-  }
+  },
 ];
 
 const menuIcon = document.getElementById('fa-bars');
@@ -118,81 +117,81 @@ menuContent.forEach((individualSection) => {
   individualSection.addEventListener('click', scrollSection);
 });
 
-/*everything below is creting elements and adding content from array*/
+/* everything below is creting elements and adding content from array */
 
-for (let i = 0; i <= projects.length - 1; i++) {
+for (let i = 0; i <= projects.length - 1; i += 1) {
   const div = document.createElement('div');
-  div.id = `dynamic-project`+ i;
-  div.className = `project project-` + i + 1;
+  div.id = `dynamic-project${i}`;
+  div.className = `project project-${i}${1}`;
   workSection.appendChild(div);
 
   const secondDiv = document.createElement('div');
-  secondDiv.id = `container` + i;
+  secondDiv.id = `container${i}`;
   secondDiv.className = 'container';
-  document.getElementById(`dynamic-project` + i).appendChild(secondDiv);
+  document.getElementById(`dynamic-project${i}`).appendChild(secondDiv);
 
   const image = document.createElement('img');
-  image.id = `image` + i;
+  image.id = `image${i}`;
   image.className = 'image-mobile';
   image.alt = 'Project picture';
   image.src = projects[i].image_mobile;
-  document.getElementById(`container` + i).appendChild(image);
+  document.getElementById(`container${i}`).appendChild(image);
 
   const dimage = document.createElement('img');
-  dimage.id = `dimage` + i;
+  dimage.id = `dimage${i}`;
   dimage.className = 'image-desktop';
   dimage.alt = 'Project picture';
   dimage.src = projects[i].image_desktop;
   document.getElementById(`container${i}`).appendChild(dimage);
 
   const h3 = document.createElement('h3');
-  h3.id = `client-name` + i;
+  h3.id = `client-name${i}`;
   h3.className = 'client-name li-mobile';
   h3.innerHTML = projects[i].project_name_mobile;
-  document.getElementById(`container` + i).appendChild(h3);
+  document.getElementById(`container${i}`).appendChild(h3);
 
   const h3desktop = document.createElement('h3');
-  h3desktop.id = `client-name-desktop` + i;
+  h3desktop.id = `client-name-desktop${i}`;
   h3desktop.className = 'client-name client-name-desktop';
   h3desktop.innerHTML = projects[i].project_name_desktop;
-  document.getElementById(`container` + i).appendChild(h3desktop);
+  document.getElementById(`container${i}`).appendChild(h3desktop);
 
   const ul = document.createElement('ul');
   const info = projects[i].project_info_mobile;
   info.forEach((individualInfoitem) => {
     const li = document.createElement('li');
-    li.id = `li` + i;
+    li.id = `li${i}`;
     li.className = 'li-mobile';
     li.innerHTML = individualInfoitem;
     ul.appendChild(li);
   });
-  document.getElementById(`container` + i).appendChild(ul);
+  document.getElementById(`container${i}`).appendChild(ul);
 
   const desktopUl = document.createElement('ul');
   const desktopinfo = projects[i].project_info_desktop;
   desktopinfo.forEach((individualdesktop) => {
     const desktopli = document.createElement('li');
-    desktopli.id = `desktopli` + i;
+    desktopli.id = `desktopli${i}`;
     desktopli.className = 'li-desktop';
     desktopli.innerHTML = individualdesktop;
     desktopUl.appendChild(desktopli);
   });
-  document.getElementById(`container` + i).appendChild(desktopUl);
+  document.getElementById(`container${i}`).appendChild(desktopUl);
 
   const p = document.createElement('p');
   p.className = 'about-project project-mobile';
   p.innerHTML = projects[i].description_mobile;
-  document.getElementById(`container` + i).appendChild(p);
+  document.getElementById(`container${i}`).appendChild(p);
 
   const desktopp = document.createElement('p');
   desktopp.className = 'about-project project-desktop';
   desktopp.innerHTML = projects[i].description_desktop;
-  document.getElementById(`container` + i).appendChild(desktopp);
+  document.getElementById(`container${i}`).appendChild(desktopp);
 
   const technologyDiv = document.createElement('div');
-  technologyDiv.id = `technologyDiv` + i;
+  technologyDiv.id = `technologyDiv${i}`;
   technologyDiv.className = 'buttons';
-  document.getElementById(`container` + i).appendChild(technologyDiv);
+  document.getElementById(`container${i}`).appendChild(technologyDiv);
 
   const technologyUl = document.createElement('ul');
   const technoLi = projects[i].technologies_mobile;
@@ -202,74 +201,67 @@ for (let i = 0; i <= projects.length - 1; i++) {
     secondLi.innerHTML = individualTechLiItem;
     technologyUl.appendChild(secondLi);
   });
-  document.getElementById(`technologyDiv` + i).appendChild(technologyUl);
+  document.getElementById(`technologyDiv${i}`).appendChild(technologyUl);
 
   const btn = document.createElement('button');
-  btn.id = `see-project` + (i + 1);
+  btn.id = `see-project${i + 1}`;
   btn.className = 'see-project';
   btn.value = i;
   btn.type = 'submit';
   btn.innerHTML = 'See project';
-  document.getElementById(`container` + i).appendChild(btn);
+  document.getElementById(`container${i}`).appendChild(btn);
 
-  const clientName = document.getElementById(`desktopli` + i);
+  const clientName = document.getElementById(`desktopli${i}`);
   clientName.classList.add('name');
-  const mobileClientName = document.getElementById(`li` + i);
+  const mobileClientName = document.getElementById(`li${i}`);
   mobileClientName.classList.add('name');
 }
 
-/*everything below is creating pop up see project*/
-
-const seeProjects = document.querySelectorAll('[id^="see-project"]');
-seeProjects.forEach((individualProject) => {
-  individualProject.addEventListener('touchstart', popupFunction);
-});
-
-
-function popupFunction (event){
+/* everything below is creating pop up see project */
+function popupFunction(event) {
   const whichbtn = event.target.value;
 
-  let locationPopup = document.getElementById('wrapperId');
+  const locationPopup = document.getElementById('wrapperId');
   locationPopup.scrollIntoView({ behavior: 'smooth' });
-  let i= whichbtn;
-  let popupPage = document.createElement('div');
-  popupPage.id = 'popPageId' + i;
+  const i = whichbtn;
+  const popupPage = document.createElement('div');
+  popupPage.id = `popPageId${i}`;
   popupPage.className = 'project popup';
   locationPopup.appendChild(popupPage);
 
-  let popupContainer = document.createElement('div');
+  const popupContainer = document.createElement('div');
   popupContainer.className = 'container';
   popupPage.appendChild(popupContainer);
 
-  let nameDiv = document.createElement('div');
+  const nameDiv = document.createElement('div');
   nameDiv.className = 'nameDiv';
   popupContainer.appendChild(nameDiv);
 
-  let popuph3 = document.createElement('h3');
-  popuph3.className= 'client-name li-mobile';
+  const popuph3 = document.createElement('h3');
+  popuph3.className = 'client-name li-mobile';
   popuph3.innerHTML = projects[i].project_name_mobile;
   nameDiv.appendChild(popuph3);
 
-  let popuph3D = document.createElement('h3');
-  popuph3D.className= 'client-name client-name-desktop';
+  const popuph3D = document.createElement('h3');
+  popuph3D.className = 'client-name client-name-desktop';
   popuph3D.innerHTML = projects[i].project_name_desktop;
   nameDiv.appendChild(popuph3D);
 
-  let iconPopup = document.createElement('i');
-  iconPopup.id = 'fa-close' + i;
-  iconPopup.className='fa fa-close';
+  const iconPopup = document.createElement('i');
+  iconPopup.id = `fa-close${i}`;
+  iconPopup.className = 'fa fa-close';
   iconPopup.value = i;
-  iconPopup.style="color: #67798e;";
+  iconPopup.style = 'color: #67798e;';
 
-  iconPopup.addEventListener('click', function() {
-    const popPage = document.getElementById('popPageId' + i);
+  iconPopup.addEventListener('click', () => {
+    const popPage = document.getElementById(`popPageId${i}`);
     if (popPage) {
       popPage.style.display = 'none';
     }
   });
   nameDiv.appendChild(iconPopup);
 
-  let popupUl = document.createElement('ul');
+  const popupUl = document.createElement('ul');
   popupUl.className = 'mobile-li';
   const popupInfo = projects[i].project_info_mobile;
   popupInfo.forEach((individualInfoitem) => {
@@ -280,7 +272,7 @@ function popupFunction (event){
   });
   popupContainer.appendChild(popupUl);
 
-  let popupUlD = document.createElement('ul');
+  const popupUlD = document.createElement('ul');
   popupUlD.className = 'desktop-li';
   const popupInfoD = projects[i].project_info_desktop;
   popupInfoD.forEach((individualInfoitemD) => {
@@ -291,35 +283,35 @@ function popupFunction (event){
   });
   popupContainer.appendChild(popupUlD);
 
-  let popupimg = document.createElement('img');
+  const popupimg = document.createElement('img');
   popupimg.className = 'image-mobile';
   popupimg.alt = 'Project picture';
   popupimg.src = projects[i].image_mobile;
   popupContainer.appendChild(popupimg);
 
-  let popupimgD = document.createElement('img');
+  const popupimgD = document.createElement('img');
   popupimgD.className = 'image-desktop';
   popupimgD.alt = 'Project picture';
   popupimgD.src = projects[i].image_desktop;
   popupContainer.appendChild(popupimgD);
 
-  let popupP = document.createElement('p');
+  const popupP = document.createElement('p');
   popupP.className = 'about-project project-mobile';
   popupP.innerHTML = projects[i].description_popup;
   popupContainer.appendChild(popupP);
 
-  let wrapperDiv = document.createElement('div');
+  const wrapperDiv = document.createElement('div');
   wrapperDiv.className = 'desktop-div';
   popupContainer.appendChild(wrapperDiv);
 
-  let popupPD = document.createElement('p');
+  const popupPD = document.createElement('p');
   popupPD.className = 'about-project project-desktop p-desktop';
   popupPD.innerHTML = projects[i].description_popup;
   wrapperDiv.appendChild(popupPD);
 
-  let popupPDe = document.createElement('p');
+  const popupPDe = document.createElement('p');
   popupPDe.className = 'about-project project-desktop';
-  popupPDe.innerHTML = projects[i].description_popup + '<br> <br>' + projects[i].description_popup + '<br> <br>';
+  popupPDe.innerHTML = `${projects[i].description_popup}<br> <br>${projects[i].description_popup}<br> <br>`;
   wrapperDiv.appendChild(popupPDe);
 
   const technDiv = document.createElement('div');
@@ -336,33 +328,34 @@ function popupFunction (event){
 
   technDiv.appendChild(techUl);
 
-  let popupSecondDiv = document.createElement('div');
+  const popupSecondDiv = document.createElement('div');
   popupSecondDiv.className = 'buttonsContainer';
   popupContainer.appendChild(popupSecondDiv);
 
+  const popupBtn = document.createElement('button');
+  popupBtn.className = 'see-project link';
+  const iconImg = document.createElement('img');
+  iconImg.src = 'images/desktop-pictures/see_live.jpg';
+  iconImg.alt = 'See live';
+  const textElement = document.createTextNode('See live');
+  popupBtn.appendChild(textElement);
+  popupBtn.appendChild(iconImg);
+  popupBtn.type = 'submit';
+  popupSecondDiv.appendChild(popupBtn);
 
-let popupBtn = document.createElement('button');
-popupBtn.className = 'see-project link';
-let iconImg = document.createElement('img');
-iconImg.src = 'images/desktop-pictures/see_live.jpg';
-iconImg.alt = 'See live';
-let textElement = document.createTextNode('See live');
-popupBtn.appendChild(textElement);
-popupBtn.appendChild(iconImg);
-popupBtn.type = 'submit';
-popupSecondDiv.appendChild(popupBtn);
+  const secongpopupBtn = document.createElement('button');
+  secongpopupBtn.className = 'see-project source';
+  const secongiconImg = document.createElement('img');
+  secongiconImg.src = 'images/desktop-pictures/see_source.jpg';
+  secongiconImg.alt = 'See live';
+  const secongtextElement = document.createTextNode('See source');
+  secongpopupBtn.appendChild(secongtextElement);
+  secongpopupBtn.appendChild(secongiconImg);
+  secongpopupBtn.type = 'submit';
+  popupSecondDiv.appendChild(secongpopupBtn);
+}
 
-
-let secongpopupBtn = document.createElement('button');
-secongpopupBtn.className = 'see-project source';
-let secongiconImg = document.createElement('img');
-secongiconImg.src = 'images/desktop-pictures/see_source.jpg';
-secongiconImg.alt = 'See live';
-let secongtextElement = document.createTextNode('See source');
-secongpopupBtn.appendChild(secongtextElement);
-secongpopupBtn.appendChild(secongiconImg);
-secongpopupBtn.type = 'submit';
-popupSecondDiv.appendChild(secongpopupBtn);
-
-
-};
+const seeProjects = document.querySelectorAll('[id^="see-project"]');
+seeProjects.forEach((individualProject) => {
+  individualProject.addEventListener('touchstart', popupFunction);
+});
